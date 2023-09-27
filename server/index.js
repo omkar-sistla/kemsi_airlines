273d9d214451d45d4590366a463e0f50ab39fcca
omkar-sistla <sosvssk@gmail.com>
2023-09-27 23:12:47 +0530

    started to work on backend

--- server/index.js
+++ server/index.js
@@ -1,2 +1,5 @@
-import express from "express";
-const app = express();
+import Express from "express";
+const app = Express();
+app.listen(3000,()=> {
+    console.log(`server is running at port ${3000}`);
+});
